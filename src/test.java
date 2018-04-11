8663f152b1f0bdf7b4f1e64df405b4c30021ce84
Aesalon <aesalon.mail@gmail.com>
2018-04-10 20:19:57 -0400

    Initial commit to GIT

---
+++ src/test.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+
+/**
+ * @author JFS
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 * 
+	public static void main(String[] args) {
+		// Chapter 1 exercises
+		int x = 0;
+		int y = 0;
+		while ( x < 5 ) {
+			if ( y < 5 ) {
+				x = x + 1;
+				if ( y < 3 ) {
+					x = x - 1;
+				}
+			}
+			y = y + 2;
+			System.out.println("x=" + x + " " + "y=" + y + " ");
+			x = x + 1;
+		}
+	}
+	 */
+	
+	/**
+	 * Chapter 1 exercise 2
+	 * 
+	public static void main (String[] args) {
+		int x = 0;
+		while ( x < 4 ) {
+			System.out.print("a");
+			if ( x < 1 ) {
+				System.out.print(" ");
+			}
+			
+			System.out.print("n");
+						
+			if ( x > 1 ) {
+				System.out.print(" oyster");
+				x = x + 2;
+			}
+			
+			if ( x == 1 ) {
+				System.out.print("noys");
+			}
+			
+			if ( x < 1 ) {
+				System.out.print("oise");
+			}
+			
+			System.out.println("");
+			x = x + 1;
+		}
+	}
+	*/
+	
+	/**
+	 * Chapter 2 exercise 1
+	 * 
+	public static void main (String[] args) {
+		// Dog test code
+		
+		Dog d = new Dog();
+		d.size = 40;
+		d.bark();
+	}
+	 */
+
+}
